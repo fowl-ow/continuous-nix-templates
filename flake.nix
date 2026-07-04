d577a397598eb46fdf08f8763ff8ca83ef79540c
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-07-04 20:54:01 +0200

    feat: add process-compose flake input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
