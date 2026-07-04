51359c9eda9d70c3872eba6a5d2c23059ab9a7e1
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-07-04 21:00:48 +0200

    refactor: use common nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,20 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    import-tree = {
+      url = "github:vic/import-tree";
+    };
+    process-compose-flake = {
+      url = "github:Platonic-Systems/process-compose-flake";
+    };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    import-tree = {
-      url = "github:vic/import-tree";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    process-compose-flake = {
-      url = "github:Platonic-Systems/process-compose-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
